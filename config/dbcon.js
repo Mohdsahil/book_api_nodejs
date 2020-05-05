5b646e719797f092db1914a48c4239f07b9b791d
Mohdsahi <sahils110043@gmail.com>
2020-05-05 17:07:08 +0530

    make it better

--- config/dbcon.js
+++ config/dbcon.js
@@ -1,7 +1,7 @@
 const { createPool } = require('mysql')
 
 const pool = createPool({
-    host: process.env.DB_HOST,
+    host: "192.168.43.39",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
